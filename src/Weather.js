174c32bddf27adcc7f6c01b7bffa19b6b11209a4
MariJ12 <mari.munizj@gmail.com>
2023-12-22 10:51:57 +0000

    linked all components

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Weather.css";
+import "./App";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Hello from the MJ world</h1>
+    </div>
+  );
+}
